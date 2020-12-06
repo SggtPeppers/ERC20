7b7b1f50c828577df8dfcd37cc896a93426ad0ff
Ramiro Fernandez <ramironfernandez@hotmail.com>
2020-12-06 10:30:52 -0300

    initial commit

--- contracts/Coke_ERC20.sol
+++ contracts/Coke_ERC20.sol
@@ -2,9 +2,16 @@ pragma solidity >=0.6.8;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title ERC20 implementation
+/// @author Ramiro Fernandez
+
+
 
 contract CokeERC20 is ERC20 {
 
+
+/// @param _name Name of the ERC20 Token
+/// @param _symbol Short name of the ERC20 Token
     constructor(
       string memory _name,
       string memory _symbol,
